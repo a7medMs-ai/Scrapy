590cbfac0d0bbdc77305a8cc6f088364a59eb36a
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-04-25 02:54:36 +0300

    Update header.py

--- app/components/header.py
+++ app/components/header.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 def header():
-    st.markdown("<h1 style='text-align: center;'>🕸️ أداة تحليل المواقع متعددة اللغات</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>🕸️ Website Localization Analysis</h1>", unsafe_allow_html=True)
