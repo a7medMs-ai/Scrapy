7cac11182abfe2cb9ba24a55225d4da9bf19d8d6
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-04-25 03:40:40 +0300

    Update settings.py

--- crawler/settings.py
+++ crawler/settings.py
@@ -1,10 +1,22 @@
-BOT_NAME = 'localization_tool'
+BOT_NAME = "localization_tool"
 
-SPIDER_MODULES = ['crawler.spiders']
-NEWSPIDER_MODULE = 'crawler.spiders'
+SPIDER_MODULES = ["crawler.spiders"]
+NEWSPIDER_MODULE = "crawler.spiders"
 
 ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {
-    'crawler.pipelines.LocalizationPipeline': 300,
+    "crawler.pipelines.LocalizationPipeline": 300,
 }
+
+FEEDS = {
+    "data/output.json": {
+        "format": "json",
+        "encoding": "utf8",
+        "store_empty": False,
+        "indent": 4,
+    }
+}
+
+DEPTH_LIMIT = 2  # prevent crawling too deep
+LOG_LEVEL = "INFO"
