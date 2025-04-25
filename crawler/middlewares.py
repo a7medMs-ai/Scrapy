df84956df009aeb0976b0e8156854b749c002272
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-04-25 03:40:05 +0300

    Update middlewares.py

--- crawler/middlewares.py
+++ crawler/middlewares.py
@@ -3,14 +3,12 @@ from scrapy import signals
 class CustomMiddleware:
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
 
     def process_request(self, request, spider):
-        # Process the request here
         return None
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info(f"Spider opened: {spider.name}")
